da93e5b63b19662a2c63d974e5285d7814fc9979
Raj Patel <rajrpatel@tryjackett.com>
2021-11-20 18:46:19 -0500

    basic structure implemented

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const path = require("path");
+const {app, ipcMain} = require("electron");
+
+const Window = require("./Window");
+
+// allows hot reload.
+require("electron-reload")(__dirname);
+
+function main() {
+    let mainWindow = new Window({
+        file: path.join(__dirname, "renderer", 'index.html')
+    });
+
+    
+}
+
+// executes the application
+app.on("ready", main);
+
+
+app.on("window-all-closed", () => {
+    app.quit();  
+})
