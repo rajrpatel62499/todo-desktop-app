f0cbc6e1827591a6dae82c650d09374900893a45
Raj Patel <rajrpatel@tryjackett.com>
2021-11-20 19:17:56 -0500

    change focus is set on input box

--- renderer/index.js
+++ renderer/index.js
@@ -1,1 +1,103 @@
-console.log("Index js ")
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => {
+        addToDo(todo)
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    addToDo();
+});
+
+input.addEventListener("keydown", function (e) {
+    var arrow = { left: 37, up: 38, right: 39, down: 40 };
+    switch (e.which) {
+        case arrow.down:
+            changeInputBoxFocus();
+          break;
+    }
+})
+
+// $('div.container').on('focus', 'li', function() {
+//     $this = $(this);
+//     $this.addClass('focus').siblings().removeClass();
+//     $this.closest('div.container').scrollTop($this.index() * $this.outerHeight());
+// }).on('keydown', 'li', function(e) {
+//     $this = $(this);
+//     if (e.keyCode == 40) {        
+//         $this.next().focus();
+//         return false;
+//     } else if (e.keyCode == 38) {        
+//         $this.prev().focus();
+//         return false;
+//     }
+// }).find('li').first().focus();
+
+
+function changeInputBoxFocus() {
+    const todosEl = document.querySelectorAll('li');
+
+    if (todosEl.length > 0) {
+        todosEl[0].focus();
+        todosEl[0].classList.add("focus");
+        input.blur();
+    }
+}
+
+
+function addToDo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
+
+
+    if (todoText) {
+        const todoEl = document.createElement('li');
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+        todoEl.innerHTML = todoText;
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+            updateLS();
+        })
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            todoEl.remove();
+            updateLS();
+        })
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+
+            text: todoEl.innerHTML,
+            completed: todoEl.classList.contains('completed')
+        })
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
